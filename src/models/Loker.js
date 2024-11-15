b88a8a311194f20d3fc7ebcd931cf298683ab678
Najla Nadiva <nadiva979@gmail.com>
2024-11-15 12:02:20 +0700

    model loker dan pengguna

---
+++ src/models/Loker.js
@@ -0,0 +1,71 @@
+import db from "../utils/db.js";
+import { Sequelize } from "sequelize";
+import Pengguna from "./Pengguna.js";
+
+const Loker = db.define("Loker", {
+    id_loker: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    tanggal_posting: {
+        type: Sequelize.DATE,
+        allowNull: false,
+    },
+    perusahaan: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    judul_loker: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    alamat: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+    },
+    posisi_loker: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    kualifikasi: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+    },
+    jenis_loker: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    deskripsi_loker: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+    },
+    kontak: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    gambar_loker: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+
+},  {tableName: "loker",
+    underscored: true,
+    timestamps: false,}
+);
+
+
+Pengguna.hasMany(Loker, {
+    foreignKey: "id_pengguna",
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+});
+
+Loker.belongsTo(Pengguna, {
+    foreignKey: "id_pengguna",
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE",
+});
+
+export default Loker;
